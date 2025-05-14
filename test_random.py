202ba3a958be93b6189d08c4f74a4e352d43f215
Lucas Cortés <lucascortesg@gmail.com>
2025-05-14 11:56:23 -0400

    renames test

--- test_random.py
+++ test_random.py
@@ -4,22 +4,21 @@ import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from scipy.sparse import hstack
 import joblib
+import time
 
-
-def main():
-    # Load full dataset
-    df = pd.read_csv("../data.csv")
-    df = df.drop("ESTADO", axis=1)
+def test_random(df, sample_size):
     df = df.dropna().dropna(axis=1)
-
     # Load saved model and encoder
-    clf = joblib.load("../rf_model.joblib")
-    encoder = joblib.load("../encoder.joblib")
+    clf = joblib.load(f"../models/lostless/rf_model_{sample_size}.joblib")
+    encoder = joblib.load(f"../models/lostless/encoder_{sample_size}.joblib")
+
+    dfi = pd.read_csv(f"../data/lostless/y_test_{sample_size}.csv")
+    indices = dfi["index"].tolist()
+    dfTest = df.iloc[indices]
 
     # Select random row
-    row = df.sample(n=1, random_state=1).copy()
-    print(row)
-
+    row = dfTest.sample(n=1, random_state=1).copy()
+    
     # Downcast types
     for col in row.select_dtypes(include=["int"]).columns:
         row[col] = pd.to_numeric(row[col], downcast="integer")
@@ -35,9 +34,19 @@ def main():
 
     # Predict
     y_pred = clf.predict(X_input)
-    print(f"Label: {y_true}, Predicted: {y_pred[0]}")
+    labels = f"Label: {y_true}, Predicted: {y_pred[0]}"
     result = "Correct" if y_true == y_pred[0] else "Incorrect"
-    print(result)
+    return labels, result
+
+
+def main():
+    # Load dataset
+    df = pd.read_csv("../data/lostless/data.csv")
+    start = time.time()
+    labels, result = test_random(df, 100000)
+    print(labels, result)
+    stop = time.time()
+    print(f"Testing time: {stop - start:.2f} seconds")
 
 
 if __name__ == "__main__":
