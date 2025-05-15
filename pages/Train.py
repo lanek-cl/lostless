e5d7aa176575d7fcd67bf9913296040b5273b79d
Lucas Cortés <lucascortesg@gmail.com>
2025-05-15 09:43:42 -0400

    updates platforma training code

--- pages/Train.py
+++ pages/Train.py
@@ -34,26 +34,47 @@ def main():
     # Streamlit app title
     st.title("Data trainer")
 
-    local = st.toggle("Local", True)
+    df = None
+    upload = st.toggle("Upload")
 
-    if local:
-        df = pd.read_csv("../data/lostless/data.csv")
-
-    else:
+    if upload:
         # File uploader
         uploaded_file = st.file_uploader("Upload an CSV file", type=["csv"])
         if uploaded_file:
             df = pd.read_csv(uploaded_file)
             df = df.dropna().dropna(axis=1)
+    else:
+        df = pd.read_csv("../lostless_data/data/data.csv")
+        df = df.dropna().dropna(axis=1)
+
     
     if df is not None:
-
-        if st.toggle("Test"):
-            all_models = os.listdir("../models/lostless/")
+        if st.toggle("Train"):
+            with st.form("training"):
+                sample_size = st.number_input(
+                    "Sample Size",
+                    min_value=100,
+                    max_value=df.shape[0],
+                    value=10000,
+                    step=10000,
+                    key="sample_size",
+                )
+                submitted = st.form_submit_button("Train Model")
+                if submitted:
+                    try:
+                        with st.spinner("Training", show_time=True):
+                            report = train(df=df, sample_size=sample_size)
+                        st.code(report)
+                        st.success("Training finished!")
+                    except Exception as e:
+                        st.error(f"An error occurred: {e}")
+        else:
+            all_models = os.listdir("../lostless_data/models/")
             model_sizes = []
             for model in all_models:
                 if "encoder" in model:
-                    model_sizes.append(model.split(".")[0].split("_")[-1])
+                    model_sizes.append(int(model.split(".")[0].split("_")[-1]))
+            model_sizes.sort()
 
             with st.form("testing"):
                 sample_size = st.selectbox(
@@ -63,31 +84,19 @@ def main():
                 
                 submitted = st.form_submit_button("Test Model")
                 if submitted:
-                    try:
+                    
+                    with st.spinner("Testing...", show_time=True):
                         labels, result = test_random(df=df, sample_size=sample_size)
-                        st.code(labels)
-                        st.code(result)
-                        st.success("Testing finished!")
-                    except Exception as e:
-                        st.error(f"An error occurred: {e}")
-        else:
-            with st.form("training"):
-                sample_size = st.number_input(
-                    "Sample Size",
-                    min_value=10000,
-                    max_value=df.shape[0],
-                    value=10000,
-                    step=10000,
-                    key="sample_size",
-                )
-                submitted = st.form_submit_button("Train Model")
-                if submitted:
+                    st.code(labels)
+                    st.code(result)
+                    file_path = f"../lostless_data/reports/classification_report_{sample_size}.txt"
                     try:
-                        report = train(df=df, sample_size=sample_size)
-                        st.code(report)
-                        st.success("Training finished!")
+                        with open(file_path, "r") as file:
+                            report = file.read()
+                            st.code(report)
                     except Exception as e:
-                        st.error(f"An error occurred: {e}")
+                        st.info(f"No report found")
+                    st.success("Testing finished!")     
 
 
 if __name__ == "__main__":
