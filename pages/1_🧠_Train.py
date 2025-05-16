26190732ea7c08aeaeed3b17cf65f52339f4734d
Lucas Cortés <lucascortesg@gmail.com>
2025-05-16 16:07:05 -0400

    updates training platform

--- pages/1_🧠_Train.py
+++ pages/1_🧠_Train.py
@@ -1,10 +1,23 @@
 import os
 import streamlit as st
 import pandas as pd
-from train import train_model
-from test_random import test_random, test_row
 import threading
 import numpy as np
+import polars as pl
+from functions import filter_data_cdm, filter_data_clc, test_row
+import subprocess
+from pathlib import Path
+import time
+
+
+def run_background_command(command):
+    subprocess.Popen(
+        command,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+        stdin=subprocess.DEVNULL,
+        start_new_session=True,
+    )
 
 
 def clear_page(title="Lanek"):
@@ -31,10 +44,10 @@ def clear_page(title="Lanek"):
         pass
 
 
-def run_train_in_background(df, sample_size, path):
-    report = train_model(df=df, sample_size=sample_size, path=path)
-    st.code(report)
-    st.write("Training completed.")
+# def run_train_in_background(df, sample_size, path):
+    # report = train_model(df=df, sample_size=sample_size, path=path)
+    # st.code(report)
+    # st.write("Training completed.")
 
 
 def get_sizes(path):
@@ -47,67 +60,135 @@ def get_sizes(path):
     return model_sizes
 
 
-def train_mode(df, path):
-    with st.form("training"):
-        #dataset = st.selectbox("Dataset", ["data_trim.csv", "data.csv"], index=0)
+def get_paths():
+    all_models = os.listdir(f"../")
+    model_sizes = []
+    for model in all_models:
+        if "lostless_data_" in model:
+            model_sizes.append(model.split("_")[-1])
+    model_sizes.sort()
+    return model_sizes
 
-        sample_size = st.number_input(
-            "Sample Size",
-            min_value=-1,
-            max_value=df.shape[0],
-            value=10000,
-            step=10000,
-            key="sample_size",
-        )
-        submitted = st.form_submit_button("Train")
-        if submitted:
-            model_sizes = get_sizes(path)
-            if sample_size not in model_sizes:
-                try:
-                    with st.spinner("Training", show_time=True):
-                        thread = threading.Thread(target=run_train_in_background, args=(df, sample_size, path))
-                        thread.start()
-                    st.info("Training running in background")
-                except Exception as e:
-                    st.error(f"An error occurred: {e}")
-            else:
-                st.info("Model of that size already exists")
+def display_log_file(file_path, lines_to_display=100, refresh_interval=1, reverse=False):
+    placeholder = st.empty()
+
+    while True:
+        # Check if file exists
+        log_file = Path(file_path)
+        if log_file.exists():
+            # Read the last `lines_to_display` lines of the file
+            with log_file.open("r") as f:
+                lines = f.readlines()[-lines_to_display:]
+                if reverse:
+                    lines = lines[::-1]
+        else:
+            lines = ["Log file not found."]
+
+        # Display the lines
+        with placeholder.container():
+            #unique_key = str(uuid.uuid4())
+            #st.text_area("Log Content", "".join(lines), height=600, key=unique_key)
+            st.code("".join(lines), height=600)
+
+        # Wait for the specified refresh interval
+        time.sleep(refresh_interval)
+
+
+def train_mode(path):
+    df = pd.read_csv(f"{path}/data/data.csv")
+    df = df.dropna().dropna(axis=1)
+    #with st.form("training"):
+    #dataset = st.selectbox("Dataset", ["data_trim.csv", "data.csv"], index=0)
+
+    sample_size = st.sidebar.number_input(
+        "Sample Size",
+        min_value=-1,
+        max_value=df.shape[0],
+        value=10000,
+        step=10000,
+        key="sample_size",
+    )
+    submitted = st.sidebar.button("Train", type="primary")
+    if submitted:
+        model_sizes = get_sizes(path)
+        if sample_size not in model_sizes:
+            try:
+                #thread = threading.Thread(target=run_train_in_background, args=(df, sample_size, path))
+                #thread.start()
+                run_background_command(
+                    [
+                        "conda",
+                        "run",
+                        "-n",
+                        "lostless",
+                        "python",
+                        f"train.py",
+                        f"{sample_size}",
+                        f"{path}",
+                    ]
+                )
+
+                st.sidebar.info("Training running in background")
+            except Exception as e:
+                st.sidebar.error(f"An error occurred: {e}")
+        else:
+            st.sidebar.warning("A model of that size already exists")
+
+    log_file_path = "results.log"
+    display_log_file(log_file_path, 25, 1, True)
 
 
 def report_mode(path):
+    model_sizes = get_sizes(path)
+    sample_size = st.sidebar.selectbox(
+        "Model Size",
+        model_sizes[::-1],
+    )
+    
+    #submitted = st.sidebar.button("Report", type="primary")
+    #if submitted:
+    report_path = f"{path}/reports/classification_report_{sample_size}.txt"
+    with st.spinner("Testing...", show_time=True):
+        try:
+            with open(report_path, "r") as file:
+                report = file.read()
+            st.code(report)
+            df = pd.read_csv(f"{path}/weights/weight_{sample_size}.csv")
+            df = df.reset_index(drop=True)
+            st.table(df)
+            hide_index_js = """
+            <script>
+                const tables = window.parent.document.querySelectorAll('table');
+                tables.forEach(table => {
+                    const indexColumn = table.querySelector('thead th:first-child');
+                    if (indexColumn) {
+                        indexColumn.style.display = 'none';
+                    }
+                    const indexCells = table.querySelectorAll('tbody th');
+                    indexCells.forEach(cell => {
+                        cell.style.display = 'none';
+                    });
+                });
+            </script>
+            """
+
+            # Use components.html to inject the JavaScript
+            st.components.v1.html(hide_index_js, height=0)
+            
+        except Exception as e:
+            st.info(f"No report found")
+        st.success("Reporting finished!")
+
+def predict_mode(path):
+    df = pd.read_csv(f"{path}/data/data.csv")
+    df = df.dropna().dropna(axis=1)
     with st.form("testing"):
         model_sizes = get_sizes(path)
         sample_size = st.selectbox(
             "Model Size",
             model_sizes[::-1],
         )
-        
-        submitted = st.form_submit_button("Report")
-        if submitted:
-            report_path = f"{path}/reports/classification_report_{sample_size}.txt"
-            with st.spinner("Testing...", show_time=True):
-                try:
-                    with open(report_path, "r") as file:
-                        report = file.read()
-                    st.code(report)
-                    df = pd.read_csv(f"{path}/weights/weight_{sample_size}.csv")
-                    st.dataframe(df)
-                    
-                except Exception as e:
-                    st.info(f"No report found")
-                st.success("Reporting finished!")
 
-def predict_mode(df, path):
-    with st.form("testing"):
-        model_sizes = get_sizes(path)
-        sample_size = st.selectbox(
-            "Model Size",
-            model_sizes[::-1],
-        )
-
-        #data = {col: np.random.choice(df[col].values) for col in df.columns}
-        #del data["ASISTIDA"]
-        #df0 = df[df["EDAD"] != 3]
         data = df.sample(n=1, random_state=sample_size).copy()
         real = data["ASISTIDA"].tolist()[0]
         data = data.drop(columns=["ASISTIDA"])
@@ -127,7 +208,7 @@ def predict_mode(df, path):
                     editable_data[key] = st.checkbox(key, value=value)
                 elif isinstance(value, float):
                     editable_data[key] = st.number_input(key, value=int(value), step=1, min_value=0)
-                elif key.startswith("FECHA") or "FECHA" in key:
+                elif (key.startswith("FECHA") or "FECHA" in key) and (key != "FECHA_DE_CITA"):
                     editable_data[key] = st.date_input(key, value=pd.to_datetime(value).date())
                 elif key.startswith("HORA") or "HORA" in key:
                     editable_data[key] = st.time_input(key, value=pd.to_datetime(value).time())
@@ -162,37 +243,31 @@ def main():
     #        df = pd.read_csv(uploaded_file)
     #        df = df.dropna().dropna(axis=1)
 
-    dataset = st.sidebar.selectbox("Dataset", ["CDM", "Old"], index=0)
-    if dataset == "CDM":
-        path = "../lostless_data_CDM"
-    elif dataset == "Old":
-        path = "../lostless_data_old"
+    mode = st.sidebar.selectbox(
+        "Option",
+        ["Train", "Report", "Filter", "Predict"],
+    )
+
+    if mode == "Filter":
+        dataset = st.sidebar.selectbox("Dataset", ["Ciudad del Mar", "Los Carrera"])
+        if dataset == "Ciudad del Mar":
+            df = pl.read_csv("../lostless_dataset/data/CDM.csv")
+            filter_data_cdm(df)
+        elif dataset == "Los Carrera":
+            dfe = pl.read_csv("../lostless_dataset/data/LCE.csv", separator=";", ignore_errors=True)
+            dfp = pl.read_csv("../lostless_dataset/data/LCP.csv", separator=";", ignore_errors=True)
+            filter_data_clc(dfe, dfp)
+
     else:
-        path = "../lostless_data"
-
-    df = pd.read_csv(f"{path}/data/data.csv")
-    df = df.dropna().dropna(axis=1)
-
-    
-    if df is not None:
-        os.makedirs(f"{path}/models", exist_ok=True)
-        os.makedirs(f"{path}/encoders", exist_ok=True)
-        os.makedirs(f"{path}/reports", exist_ok=True)
-        os.makedirs(f"{path}/weights", exist_ok=True)
-        mode = st.sidebar.selectbox(
-            "Option",
-            ["Predict", "Report", "Train"],
-        )
+        dataset = st.sidebar.selectbox("Dataset", get_paths(), index=0)
+        path = f"../lostless_data_{dataset}"
         if mode == "Train":
-            train_mode(df, path)
-
+            train_mode(path)
         elif mode == "Report":
             report_mode(path)
-        
         elif mode == "Predict":
-            predict_mode(df, path)
-                 
+            predict_mode(path)
 
-
+    
 if __name__ == "__main__":
     main()
