b857e54405ace875e16d5fcf18fb7b248061313a
Lucas Cortés <lucascortesg@gmail.com>
2025-05-16 16:06:08 -0400

    simplifies test file

---
+++ test.py
@@ -0,0 +1,19 @@
+# predict_random.py
+import time
+import sys
+from functions import test_random
+
+
+def main():
+    # Load dataset
+    sample_size = int(sys.argv[1])
+    path = sys.argv[2]
+    start = time.time()
+    labels, result = test_random(sample_size, path)
+    print(labels, result)
+    stop = time.time()
+    print(f"Testing time: {stop - start:.2f} seconds")
+
+
+if __name__ == "__main__":
+    main()
